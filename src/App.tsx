3317ef562aef8414582f05c35487b787ad358a38
MateoAltamirano <mateo.altamirano.1998@gmail.com>
2021-04-17 02:13:15 -0400

    feat: add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { Button } from "@chakra-ui/react";
 import "./styles/App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <div className="App">
-      <Button>Crunchy List</Button>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Switch>
+        <Button>Crunchy List</Button>
+      </Router>
     </div>
   );
 };
