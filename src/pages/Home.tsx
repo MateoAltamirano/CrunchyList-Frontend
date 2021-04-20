6eb08bed12ccc479efc1fd101c60476cb3733855
MateoAltamirano <mateo.altamirano.1998@gmail.com>
2021-04-20 16:24:04 -0400

    feat: add home basic carousel layouts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import { useContext } from "react";
+import Slider from "react-slick";
 import Card from "../components/Card";
 import { userContext } from "../providers/UserContext";
 import "../styles/home.css";
@@ -8,6 +9,42 @@ const Home = () => {
   const user = useContext(userContext);
   if (user === undefined)
     throw new Error("Please use within UserContextProvider");
+  const popularCarouselSettings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+  };
+  const recommendedCarouselSettings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          infinite: true,
+          speed: 500,
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          infinite: true,
+          speed: 500,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          infinite: true,
+          speed: 500,
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
   const { isAuthenticated } = user.state;
   return (
     <Box h="100%">
@@ -19,22 +56,60 @@ const Home = () => {
           La base de datos de anime más grande del mundo
         </Text>
       </Box>
-      <Flex position="absolute" top={"75%"} w={"100%"}>
-        <Card w={"100%"} margin={"0 3rem"} marginBottom="60px">
+      <Flex position="absolute" top={"75%"} w={"100%"} padding="0 3rem">
+        <Card w={"100%"} marginBottom="60px">
           <Heading size="lg" alignSelf="self-start" color="gray.700">
             Populares
           </Heading>
           <Text alignSelf="self-start" fontSize="md">
             Los animes más populares de la temporada te esperan
           </Text>
+          <Box w={"100%"}>
+            <Slider {...popularCarouselSettings}>
+              <Box h={"25rem"} w={"25rem"} bgColor="red">
+                1
+              </Box>
+              <Box h={"25rem"} w={"25rem"} bgColor="blue">
+                2
+              </Box>
+              <Box h={"25rem"} w={"25rem"} bgColor="green">
+                3
+              </Box>
+              <Box h={"25rem"} w={"25rem"} bgColor="yellow">
+                4
+              </Box>
+              <Box h={"25rem"} w={"25rem"} bgColor="pink">
+                5
+              </Box>
+            </Slider>
+          </Box>
           {isAuthenticated ? (
-            <Box w={"100%"}>
+            <Box w={"100%"} marginTop="1rem">
               <Heading size="lg" alignSelf="self-start" color="gray.700">
                 Recomendados
               </Heading>
               <Text alignSelf="self-start" fontSize="md">
                 Los animes más recomendados en base a tus gustos personales
               </Text>
+              <Box w={"100%"}>
+                <Slider {...recommendedCarouselSettings}>
+                  <Box h={"25rem"} w={"25rem"} bgColor="red">
+                    1
+                  </Box>
+                  <Box h={"25rem"} w={"25rem"} bgColor="blue">
+                    2
+                  </Box>
+                  <Box h={"25rem"} w={"25rem"} bgColor="green">
+                    3
+                  </Box>
+                  <Box h={"25rem"} w={"25rem"} bgColor="yellow">
+                    4
+                  </Box>
+                  <Box h={"25rem"} w={"25rem"} bgColor="pink">
+                    5
+                  </Box>
+                </Slider>
+              </Box>
             </Box>
           ) : undefined}
         </Card>
