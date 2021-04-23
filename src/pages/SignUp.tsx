920e561e2c1172ba82c9877250d11bd38bc98ce2
MateoAltamirano <mateo.altamirano.1998@gmail.com>
2021-04-22 21:17:28 -0400

    fix: clean warnings

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,29 +1,15 @@
-import {
-  Box,
-  Input,
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-} from "@chakra-ui/react";
-import { useContext, useEffect } from "react";
+import { Box, Input, Button } from "@chakra-ui/react";
+import { useContext } from "react";
 import { userContext } from "../providers/UserContext";
-import { UserActionType } from "../reducers/UserReducer";
 import { useForm } from "react-hook-form";
 import "../styles/login.css";
 import "../styles/forms.css";
 import logo from "../assets/img/LogoWhiteMAL.png";
-import {
-  Link, Router,
-} from 'react-router-dom';
-import { useCallback } from "react";
-import {createUser} from '../api/user'
+import { Link } from "react-router-dom";
+import { createUser } from "../api/user";
 import { IUser } from "../models/";
-import axios, { AxiosResponse, AxiosError } from "axios";
+import { AxiosResponse, AxiosError } from "axios";
 import { useHistory } from "react-router-dom";
-
 
 const SignUp = () => {
   const history = useHistory();
@@ -38,29 +24,30 @@ const SignUp = () => {
   } = useForm();
 
   const onSubmit = (data: IUser) => {
-    console.log(data)
+    console.log(data);
     //let createdUser = createUser;
     let res = createUser;
-    res(data).then((response: AxiosResponse) => {
-      console.log(response);
-      if(response.status==200){
-        alert("Usuario creado")
-        history.push("/login");
-      }
-      // dispatch({
-      //   type: UserActionType.SET_INFO,
-      //   user: { ...response.data, status: Status.SUCCESS },
-      // });
-    })
-    .catch((error: AxiosError) => {
-      console.log(error.message);
-    });
+    res(data)
+      .then((response: AxiosResponse) => {
+        console.log(response);
+        if (response.status === 200) {
+          alert("Usuario creado");
+          history.push("/login");
+        }
+        // dispatch({
+        //   type: UserActionType.SET_INFO,
+        //   user: { ...response.data, status: Status.SUCCESS },
+        // });
+      })
+      .catch((error: AxiosError) => {
+        console.log(error.message);
+      });
   };
 
   return (
     <Box className="bg">
       <Box backgroundColor={"primary.main"} className="loginCard">
-        <img src={logo}></img>
+        <img alt={"logo"} src={logo} />
         <h1>Crear Cuenta</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
           <Box className={"input-box"}>
@@ -71,7 +58,7 @@ const SignUp = () => {
             />
             {errors.nombre && <span>{errors.nombre.message}</span>}
           </Box>
-          
+
           <Box className={"input-box"}>
             <Input
               type={"text"}
@@ -80,7 +67,7 @@ const SignUp = () => {
             />
             {errors.username && <span>{errors.username.message}</span>}
           </Box>
-          
+
           <Box className={"input-box"}>
             <Input
               type={"text"}
@@ -88,38 +75,50 @@ const SignUp = () => {
               {...register("correo", {
                 required: "Debe ingresar un email",
                 pattern: {
-                  value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                  value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
                   message: "Debe ingresar un email válido",
                 },
               })}
             />
             {errors.correo && <span>{errors.correo.message}</span>}
           </Box>
-          
+
           <Box className={"input-box"}>
             <Input
               type={"password"}
               placeholder={"Contraseña"}
-              {...register("password", { required: "Debe incresar una contraseña" })}
+              {...register("password", {
+                required: "Debe incresar una contraseña",
+              })}
             />
             {errors.password && <span>{errors.password.message}</span>}
           </Box>
-          
+
           <Box className={"input-box"}>
             <Input
               type={"date"}
               placeholder={"Fecha de nacimiento"}
-              {...register("fechaNacimiento", { required: "Debe ingresar la fecha de nacimiento" })}
+              {...register("fechaNacimiento", {
+                required: "Debe ingresar la fecha de nacimiento",
+              })}
             />
-            {errors.fechaNacimiento && <span>{errors.fechaNacimiento.message}</span>}
+            {errors.fechaNacimiento && (
+              <span>{errors.fechaNacimiento.message}</span>
+            )}
           </Box>
-          
-          <Button marginTop={"10px"} backgroundColor={"primary.dark"} type={"submit"}>
+
+          <Button
+            marginTop={"10px"}
+            backgroundColor={"primary.dark"}
+            type={"submit"}
+          >
             Ingresar
           </Button>
         </form>
         <p>Ya tiene una cuenta?</p>
-        <Link style={{color: "white"}} to="/login">Iniciar Sesión</Link>
+        <Link style={{ color: "white" }} to="/login">
+          Iniciar Sesión
+        </Link>
       </Box>
     </Box>
   );
