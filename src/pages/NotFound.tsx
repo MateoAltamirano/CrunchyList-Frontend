d725b955158bc80f1cd2daa8ab35f39cbc6c2e2b
MateoAltamirano <mateo.altamirano.1998@gmail.com>
2021-04-27 15:55:21 -0400

    feat: not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,23 @@
+import { Box, Flex, Heading, Text } from "@chakra-ui/layout";
+import { BiMessageSquareError } from "react-icons/bi";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <Flex
+      h="100%"
+      w="100%"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+    >
+      <BiMessageSquareError fontSize="10rem" />
+      <Heading size="3xl">Oops!</Heading>
+
+      <Text m="1rem 5rem" fontSize="2xl">
+        No pudimos encontrar la página que estas buscando
+      </Text>
+    </Flex>
+  );
 };
 
 export default NotFound;
